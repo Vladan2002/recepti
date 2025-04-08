70db1ecc015dc3c4e01c76960e6e2d48b7b7a9f3
vlado <vladancmiljanic123@gmail.com>
2025-04-08 15:28:00 +0200

    MVC first version

---
+++ scripts/card.js
@@ -0,0 +1,75 @@
+var urlParams = new URLSearchParams(window.location.search);
+var id = urlParams.get('id');
+
+async function fetchCard() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var id = urlParams.get('id');
+
+    loader(true)
+
+    if (!id) {
+        document.getElementById("recipe").innerHTML = "<p>No recipe ID provided.</p>";
+        return;
+    }
+
+    const options = {
+        method: 'GET',
+        url: 'https://tasty.p.rapidapi.com/recipes/get-more-info',
+        params: { id: id },
+        headers: {
+            'x-rapidapi-key': '95da5eb655msh9dc84ffae7afa48p1b51b9jsn1986d4b5c662',
+            'x-rapidapi-host': 'tasty.p.rapidapi.com'
+        }
+    };
+
+    try {
+        var recipeResponse = await axios.get('/view/partials/recipe.hbs');
+        var specsResponse = await axios.get('/view/partials/specs.hbs');
+
+        var recipeTemplate = Handlebars.compile(recipeResponse.data);
+        var specsTemplate = Handlebars.compile(specsResponse.data);
+
+        var response = await axios.request(options);
+        var data = response.data;
+
+        var specsData = {
+            title: data.name,
+            calories: data.nutrition?.calories,
+            carbs: data.nutrition?.carbohydrates ,
+            fat: data.nutrition?.fat ,
+            protein: data.nutrition?.protein ,
+            sugar: data.nutrition?.sugar
+        };
+
+        var recipeData = {
+            picture: data.thumbnail_url,
+            recipe: data.instructions.map((instr, index) => ({
+                step: index + 1,
+                instruction: instr.display_text
+            }))
+        };
+
+        loader(false)
+
+        document.getElementById("specs").innerHTML = specsTemplate(specsData);
+        document.getElementById("recipe").innerHTML = recipeTemplate(recipeData);
+
+    } catch (err) {
+        console.error("Error fetching recipe details:", err);
+        document.getElementById("recipe").innerHTML = "<p>Failed to load recipe details.</p>";
+    }
+}
+
+function loader(boolean) {
+
+    if (boolean) {
+        document.getElementById('loader').style.display = 'flex';
+    }else{
+        document.getElementById('loader').style.display = 'none';
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    fetchCard();
+})
---
+++ scripts/script.js
@@ -0,0 +1,103 @@
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await loadElements();
+    await fetchData()
+})
+
+
+
+
+
+async function fetchData() {
+
+    loader(true);
+    q=document.getElementById('search').value
+    const params = {
+        from: '0',
+        size: '20'
+    };
+
+    if (q) {
+        params.q = q;
+    }
+    const request = {
+        method: "GET",
+        url: "https://tasty.p.rapidapi.com/recipes/list",
+            params: params
+        ,
+        headers: {
+            'x-rapidapi-key': '95da5eb655msh9dc84ffae7afa48p1b51b9jsn1986d4b5c662',
+            'x-rapidapi-host': 'tasty.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const partialResponse = await axios.get('/view/partials/card.hbs');
+        const partialText = partialResponse.data;
+
+        const template = Handlebars.compile(partialText);
+
+        const response = await axios.request(request);
+        const results = response.data.results;
+
+        let cardsHTML = "";
+
+        for (let i = 0; i < results.length; i++) {
+            const cardData = {
+                id: results[i].id,
+                name: results[i].name,
+                picture: results[i].thumbnail_url,
+                tag: results[i].topics?.[0]?.name || "",
+                time: results[i].total_time_minutes ? `${results[i].total_time_minutes} min` : ""
+            };
+
+            cardsHTML += template(cardData);
+        }
+        loader(false);
+        document.getElementById("content").innerHTML = cardsHTML;
+
+    } catch (err) {
+        console.error("Error fetching data:", err);
+        document.getElementById("content").innerHTML = "<p>Failed to load recipes.</p>";
+    }
+}
+
+
+
+
+function card(id) {
+
+        window.location.href = `http://127.0.0.1:8080/card.html?id=${id}`;
+
+
+}
+
+function loader(boolean) {
+
+    if (boolean) {
+        document.getElementById('loader').style.display = 'flex';
+    }else{
+        document.getElementById('loader').style.display = 'none';
+    }
+
+}
+
+async function loadElements() {
+    try {
+        var headerResponse = await axios.get('/view/partials/index-header.hbs');
+       var navbarResponse = await axios.get('/view/partials/navbar.hbs');
+
+        var headerTemplate = Handlebars.compile(headerResponse.data);
+        var navbarTemplate = Handlebars.compile(navbarResponse.data);
+        console.log(headerTemplate);
+
+        document.getElementById('navbar').innerHTML = navbarTemplate();
+        document.getElementById('container').innerHTML = headerTemplate();
+
+
+    } catch (e) {
+
+    }
+}
+
+
